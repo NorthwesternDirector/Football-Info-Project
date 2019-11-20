f968a639ee34df019cd4ada471d9c078c8b26877
NorthwesternDirector <fanshundee@126.com>
2019-11-20 20:26:58 +0800

    feat: 将球队logo和name封装为组件

---
+++ src/components/TeamCard/TeamLogoName.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Card, Avatar, Row, Col, Tag } from 'antd'
+
+const TeamLoName = ({ matchDetail }) =>
+    <Row gutter={24}>
+      {matchDetail.map(val =>
+        <Col span={12} style={{ marginBottom: '8px' }}>
+          <Card title={val.match } description={val.status} extra={<a href={val.rateProgress.type[0].href} target="_blank">比赛战报</a>}>
+            <Row gutter={24}>
+              <Col span={8} style={{ textAlign: 'center' }}>
+                <Avatar shape="square" size={74} src={val.rateProgress.matchData[0].icon} style={{ display: 'block', margin: '0 auto' }}/>
+                {(val.rateProgress.matchData[0].rate >= val.rateProgress.matchData[1].rate) ?
+                  <Tag color="volcano" style={{ display: 'inline-block', margin: '5px 0 0 0' }}>{val.rateProgress.matchData[0].name}</Tag>
+                  : <Tag color="cyan" style={{ display: 'inline-block', margin: '5px 0 0 0' }}>{val.rateProgress.matchData[0].name}</Tag>
+                }
+              </Col>
+              <Col span={8} style={{ textAlign: 'center' }}>
+                {`${val.status}`}
+                <h1 style={{ color: '#FF8C00', fontSize: '30px', margin: '0' }}> {`${val.rateProgress.matchData[0].rate}:${val.rateProgress.matchData[1].rate}`}</h1>
+                {`${val.rateProgress.matchData[0].startTime} ${val.rateProgress.matchData[1].startTime}`}
+              </Col>
+              <Col span={8} style={{ textAlign: 'center' }}>
+                <Avatar shape="square" size={74} src={val.rateProgress.matchData[1].icon} style={{ display: 'block', margin: '0 auto' }}/>
+                {(val.rateProgress.matchData[0].rate >= val.rateProgress.matchData[1].rate) ?
+                  <Tag color="cyan" style={{ display: 'inline-block', margin: '5px auto 0' }}>{val.rateProgress.matchData[1].name}</Tag>
+                  : <Tag color="volcano" style={{ display: 'inline-block', margin: '5px 0 0 0' }}>{val.rateProgress.matchData[1].name}</Tag>
+                }
+              </Col>
+            </Row>
+          </Card>
+        </Col>,
+      )}
+    </Row>
+
+
+export default React.memo(TeamLoName, (pre, next) => JSON.stringify(pre) === JSON.stringify(next))
--- src/pages/matchInfo/index.jsx
+++ src/pages/matchInfo/index.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react'
 
-import { Tabs, Card, Skeleton, Avatar, Row, Col, Tag } from 'antd'
+import { Tabs } from 'antd'
 import { connect } from 'dva'
+import TeamLoName from '../../components/TeamCard/TeamLogoName'
 
 
 const MatchInfo = props => {
   const { dispatch, matchDetail } = props
 
-  const [league, setLeague] = useState('seri')
+  const [league, setLeague] = useState('')
   console.log(matchDetail)
   useEffect(() => {
     dispatch({
@@ -20,111 +21,29 @@ const MatchInfo = props => {
     setLeague(key)
   }
 
+  const leagueList = [
+    { tab: '全部', key: '' },
+    { tab: '德甲', key: 'bund' },
+    { tab: '意甲', key: 'seri' },
+    { tab: '西甲', key: 'liga' },
+    { tab: '法甲', key: 'fran' },
+    { tab: '英超', key: 'epl' },
+    { tab: '欧冠', key: 'chlg' },
+    { tab: '欧联', key: 'uefael' },
+    { tab: '中超', key: 'csl' },
+
+  ]
+
   return (
     <>
-      <Tabs defaultActiveKey="1" onChange={callback}>
-        <Tabs.TabPane tab="意甲" key="seri">
-        <Row gutter={24}>
-          { matchDetail ?
-          (matchDetail.map(val =>
-            <Col span={12} style={{ marginBottom: '8px' }}>
-              <Card title={val.match } description={val.status} extra={<a href={val.rateProgress.type[0].href} target="_blank">比赛战报</a>}>
-                <Row gutter={24}>
-                  <Col span={8} style={{ textAlign: 'center' }}>
-                    <Avatar shape="square" size={74} src={val.rateProgress.matchData[0].icon} style={{ display: 'block', margin: '0 auto' }}/>
-                    {(val.rateProgress.matchData[0].rate >= val.rateProgress.matchData[1].rate) ?
-                      <Tag color="volcano" style={{ display: 'inline-block', margin: '5px 0 0 0' }}>{val.rateProgress.matchData[0].name}</Tag>
-                      : <Tag color="cyan" style={{ display: 'inline-block', margin: '5px 0 0 0' }}>{val.rateProgress.matchData[0].name}</Tag>
-                    }
-                  </Col>
-                  <Col span={8} style={{ textAlign: 'center' }}>
-                    {`${val.status}`}
-                    <h1 style={{ color: '#FF8C00', fontSize: '30px', margin: '0' }}> {`${val.rateProgress.matchData[0].rate}:${val.rateProgress.matchData[1].rate}`}</h1>
-                    {`${val.rateProgress.matchData[0].startTime} ${val.rateProgress.matchData[1].startTime}`}
-                  </Col>
-                  <Col span={8} style={{ textAlign: 'center' }}>
-                    <Avatar shape="square" size={74} src={val.rateProgress.matchData[1].icon} style={{ display: 'block', margin: '0 auto' }}/>
-                    {(val.rateProgress.matchData[0].rate >= val.rateProgress.matchData[1].rate) ?
-                      <Tag color="cyan" style={{ display: 'inline-block', margin: '5px auto 0' }}>{val.rateProgress.matchData[1].name}</Tag>
-                      : <Tag color="volcano" style={{ display: 'inline-block', margin: '5px 0 0 0' }}>{val.rateProgress.matchData[1].name}</Tag>
-                    }
-                  </Col>
-                </Row>
-              </Card>
-            </Col>,
-         ))
-          : <Skeleton title={false} paragraph={{ rows: 6 }}></Skeleton>
-          }
-        </Row>
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="西甲" key="liga">
-        <Row gutter={24}>
-          { matchDetail ?
-          (matchDetail.map(val =>
-            <Col span={12} style={{ marginBottom: '8px' }}>
-              <Card title={val.match } description={val.status} extra={<a href={val.rateProgress.type[0].href} target="_blank">比赛战报</a>}>
-                <Row gutter={24}>
-                  <Col span={8} style={{ textAlign: 'center' }}>
-                    <Avatar shape="square" size={74} src={val.rateProgress.matchData[0].icon} style={{ display: 'block', margin: '0 auto' }}/>
-                    {(val.rateProgress.matchData[0].rate >= val.rateProgress.matchData[1].rate) ?
-                      <Tag color="volcano" style={{ display: 'inline-block', margin: '5px 0 0 0' }}>{val.rateProgress.matchData[0].name}</Tag>
-                      : <Tag color="cyan" style={{ display: 'inline-block', margin: '5px 0 0 0' }}>{val.rateProgress.matchData[0].name}</Tag>
-                    }
-                  </Col>
-                  <Col span={8} style={{ textAlign: 'center' }}>
-                    {`${val.status}`}
-                    <h1 style={{ color: '#FF8C00', fontSize: '30px', margin: '0' }}> {`${val.rateProgress.matchData[0].rate}:${val.rateProgress.matchData[1].rate}`}</h1>
-                    {`${val.rateProgress.matchData[0].startTime} ${val.rateProgress.matchData[1].startTime}`}
-                  </Col>
-                  <Col span={8} style={{ textAlign: 'center' }}>
-                    <Avatar shape="square" size={74} src={val.rateProgress.matchData[1].icon} style={{ display: 'block', margin: '0 auto' }}/>
-                    {(val.rateProgress.matchData[0].rate >= val.rateProgress.matchData[1].rate) ?
-                      <Tag color="cyan" style={{ display: 'inline-block', margin: '5px auto 0' }}>{val.rateProgress.matchData[1].name}</Tag>
-                      : <Tag color="volcano" style={{ display: 'inline-block', margin: '5px 0 0 0' }}>{val.rateProgress.matchData[1].name}</Tag>
-                    }
-                  </Col>
-                </Row>
-              </Card>
-            </Col>,
-         ))
-          : <Skeleton title={false} paragraph={{ rows: 6 }}></Skeleton>
-          }
-        </Row>
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="中超" key="csl">
-        <Row gutter={24}>
-          { matchDetail ?
-          (matchDetail.map(val =>
-            <Col span={12} style={{ marginBottom: '8px' }}>
-              <Card title={val.match } description={val.status} extra={<a href={val.rateProgress.type[0].href} target="_blank">比赛战报</a>}>
-                <Row gutter={24}>
-                  <Col span={8} style={{ textAlign: 'center' }}>
-                    <Avatar shape="square" size={74} src={val.rateProgress.matchData[0].icon} style={{ display: 'block', margin: '0 auto' }}/>
-                    {(val.rateProgress.matchData[0].rate >= val.rateProgress.matchData[1].rate) ?
-                      <Tag color="volcano" style={{ display: 'inline-block', margin: '5px 0 0 0' }}>{val.rateProgress.matchData[0].name}</Tag>
-                      : <Tag color="cyan" style={{ display: 'inline-block', margin: '5px 0 0 0' }}>{val.rateProgress.matchData[0].name}</Tag>
-                    }
-                  </Col>
-                  <Col span={8} style={{ textAlign: 'center' }}>
-                    {`${val.status}`}
-                    <h1 style={{ color: '#FF8C00', fontSize: '30px', margin: '0' }}> {`${val.rateProgress.matchData[0].rate}:${val.rateProgress.matchData[1].rate}`}</h1>
-                    {`${val.rateProgress.matchData[0].startTime} ${val.rateProgress.matchData[1].startTime}`}
-                  </Col>
-                  <Col span={8} style={{ textAlign: 'center' }}>
-                    <Avatar shape="square" size={74} src={val.rateProgress.matchData[1].icon} style={{ display: 'block', margin: '0 auto' }}/>
-                    {(val.rateProgress.matchData[0].rate >= val.rateProgress.matchData[1].rate) ?
-                      <Tag color="cyan" style={{ display: 'inline-block', margin: '5px auto 0' }}>{val.rateProgress.matchData[1].name}</Tag>
-                      : <Tag color="volcano" style={{ display: 'inline-block', margin: '5px 0 0 0' }}>{val.rateProgress.matchData[1].name}</Tag>
-                    }
-                  </Col>
-                </Row>
-              </Card>
-            </Col>,
-         ))
-          : <Skeleton title={false} paragraph={{ rows: 6 }}></Skeleton>
-          }
-        </Row>
-        </Tabs.TabPane>
+      <Tabs defaultActiveKey="" onChange={callback}>
+        {
+          leagueList.map(item =>
+            <Tabs.TabPane tab={item.tab} key={item.key}>
+              <TeamLoName matchDetail={matchDetail}></TeamLoName>
+            </Tabs.TabPane>,
+          )
+        }
       </Tabs>
     </>
   )
