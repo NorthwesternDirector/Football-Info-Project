d7a5b3dd60702046bc68f83431c8157a74566f30
NorthwesternDirector <fanshundee@126.com>
2019-11-21 23:15:01 +0800

    feat: 积分情况按联赛分类

--- src/pages/pointsInfo/index.jsx
+++ src/pages/pointsInfo/index.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 
-import { Tabs, Table } from 'antd'
+import { Tabs, Table, Avatar } from 'antd'
 import { connect } from 'dva'
 
 
 const MatchInfo = props => {
   const { dispatch, pointsInfoPage, loading } = props
-  const [league, setLeague] = useState('France')
+  const [league, setLeague] = useState('14225')
 
   const leagueList = [
     { tab: '中超', key: '14225' },
@@ -17,17 +17,15 @@ const MatchInfo = props => {
     { tab: '英超', key: '14931' },
   ]
 
-  const params = {
-    season_id: '14225',
-    app: 'dqd',
-    version: 0,
-    platform: 'web',
-  }
-
   useEffect(() => {
     dispatch({
       type: 'pointsInfo/fetchPointInfo',
-      payload: params,
+      payload: {
+        season_id: league,
+        app: 'dqd',
+        version: 0,
+        platform: 'web',
+      },
     })
     return () => dispatch({
       type: 'pointsInfo/clear',
@@ -38,22 +36,38 @@ const MatchInfo = props => {
     setLeague(key)
   }
 
-  const data = pointsInfoPage && pointsInfoPage.data ? pointsInfoPage.data.map(item => ({
+  const data = pointsInfoPage && pointsInfoPage.data ? pointsInfoPage.data.map((item, index) => ({
     key: item.team_name,
+    排名: index + 1,
     球队: item.team_name,
     赛: item.matches_total,
     胜: item.matches_won,
     平: item.matches_draw,
     负: item.matches_lost,
-    '进/失': `${item.goals_pro}/${item.goals_against}`,
-    积分: item.points }),
+    进球: item.goals_pro,
+    失球: item.goals_against,
+    净胜球: item.goals_pro - item.goals_against,
+    积分: item.points,
+    team_logo: item.team_logo,
+   }),
   ) : []
 
   const columns = [
     {
+      title: '排名',
+      dataIndex: '排名',
+      key: '排名',
+    },
+    {
       title: '球队',
       dataIndex: '球队',
       key: '球队',
+      render: (text, record) => (
+        <>
+        <Avatar shape="square" src={record.team_logo} style={{ marginRight: '8px' }}/>
+        {record.key}
+        </>
+      ),
     },
     {
       title: '赛',
@@ -76,9 +90,19 @@ const MatchInfo = props => {
       key: '负',
     },
     {
-      title: '进/失',
-      dataIndex: '进/失',
-      key: '进/失',
+      title: '进球',
+      dataIndex: '进球',
+      key: '进球',
+    },
+    {
+      title: '失球',
+      dataIndex: '失球',
+      key: '失球',
+    },
+    {
+      title: '净胜球',
+      dataIndex: '净胜球',
+      key: '净胜球',
     },
     {
       title: '积分',
