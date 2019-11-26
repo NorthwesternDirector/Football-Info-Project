6af906467cd95f9c1179eb5b1bf3fe4b69865607
NorthwesternDirector <fanshundee@126.com>
2019-11-26 20:59:53 +0800

    feat: 积分详情页添加表格统计

--- src/pages/scheduleDetail/index.jsx
+++ src/pages/scheduleDetail/index.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useMemo } from 'react'
+import ReactEcharts from 'echarts-for-react'
+import moment from 'moment'
 
-import { Tabs, Table, Avatar, Card } from 'antd'
+import { Avatar, Card } from 'antd'
 import { connect } from 'dva'
 
 
@@ -13,12 +15,88 @@ const ScheduleInfo = props => {
     })
   }, [])
 
+  const Line = ({
+    title,
+    data,
+    category,
+    chartHeight,
+  }) => {
+    const option = {
+      title: {
+        text: title,
+        x: 'center',
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow',
+        },
+      },
+      toolbox: {
+        show: true,
+        right: 15,
+        top: 'top',
+        feature: {
+          magicType: {
+            type: ['stack', 'tiled'],
+          },
+        },
+      },
+      legend: {
+        data: data.name,
+        top: 50,
+        x: 'right',
+        orient: 'vertical',
+      },
+      dataZoom: [
+        {
+          show: true,
+          realtime: true,
+        },
+      ],
+      grid: {
+        top: 40,
+        left: 20,
+        right: 100,
+        bottom: 50,
+        containLabebl: true,
+      },
+      xAxis: {
+        type: 'category',
+        data: category,
+
+      },
+      yAxis: {
+        type: 'value',
+        boundaryGap: [0, 0.01],
+      },
+      series: [
+        {
+          type: 'line',
+          data: data.map(item => ([item.data.slice(0, 10), item.totalScore])),
+          smooth: true,
+        },
+      ],
+    }
+    return <ReactEcharts theme="theme" option={option} style={{ height: chartHeight }}></ReactEcharts>
+  }
+
   const { data } = schedule
+
+  const selectDate = useMemo(() => {
+    const selectArr = data ? data.timeRange.split(',') : ['2019-08-17', '2019-11-23']
+    const arrayLength = moment(selectArr[1]).diff(moment(selectArr[0]), 'days')
+
+    return new Array(arrayLength).fill(1).map((val, index, array) =>
+      moment().subtract(array.length - index, 'days').format('YYYY-MM-DD'),
+    )
+  }, [data])
 
   return (
     <>
       {data ? <Card title={data.team} loading={loading}>
         <Avatar shape="square" size="large" src={data.logo}></Avatar>
+        <Line title="西甲积分情况" data={data.list} category={selectDate} chartHeight={400}></Line>
       </Card> : null}
 
 
