9e64a5889ad8680f5bcd046f0624a23593390bf9
NorthwesternDirector <fanshundee@126.com>
2019-11-28 20:18:32 +0800

    feat: 封装词云

---
+++ src/components/Charts/WordCloud.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Skeleton } from 'antd'
+import echarts from 'echarts'
+import ReactEchartsCore from 'echarts-for-react/lib/core'
+import 'echarts-wordcloud'
+
+const WordCloud = ({ data, onEvents, chartHeight = 300 }) => {
+  if (!data) return <Skeleton title={false} />
+  const formatData = data
+  if (formatData.length === 0) return <Skeleton title={false} />
+  const option = {
+    tooltip: {
+      show: false,
+      trigger: 'item',
+      formatter(params) {
+        return `${params.name}:${params.value}`
+      },
+    },
+
+    series: [
+      {
+        type: 'wordCloud',
+        shape: 'circle',
+        maskImage: null,
+        left: 'center',
+        top: 'center',
+        width: '95%',
+        height: '95%',
+        right: null,
+        bottom: null,
+        sizeRange: [12, 60],
+        rotationRange: [0, 0],
+        rotationStep: 0,
+        gridSize: 8,
+        drawOutOfBound: false,
+        textStyle: {
+          normal: {
+            fontFamily: 'sans-serif',
+            fontWeight: 'bold',
+            color() {
+              return `rgb(${[
+                Math.round(Math.random() * 200),
+                Math.round(Math.random() * 200),
+                Math.round(Math.random() * 200),
+              ].join(',')})`
+            },
+          },
+          emphasis: {
+            shadowBlur: 2,
+            shadowColor: '#333',
+          },
+        },
+        // Data is an array. Each array item must have name and value property.
+        data: formatData,
+      },
+    ],
+  }
+  return (
+    <ReactEchartsCore
+      theme="theme"
+      echarts={echarts}
+      option={option}
+      notMerge
+      onEvents={onEvents}
+      style={{ height: chartHeight }}
+    />
+  )
+}
+
+export default WordCloud
