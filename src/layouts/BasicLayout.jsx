d57928e513daabf4d4af025ca66f706dd5b3442c
NorthwesternDirector <fanshundee@126.com>
2019-11-29 09:43:03 +0800

    feat: 修改系统logo

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -12,7 +12,8 @@ import { formatMessage } from 'umi-plugin-react/locale';
 import Authorized from '@/utils/Authorized';
 import RightContent from '@/components/GlobalHeader/RightContent';
 import { isAntDesignPro, getAuthorityFromRouter } from '@/utils/utils';
-import logo from '../assets/logo.svg';
+import logo from '../assets/logo2.png';
+
 const noMatch = (
   <Result
     status="403"
