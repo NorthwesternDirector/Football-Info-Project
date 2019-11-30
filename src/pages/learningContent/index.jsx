11f3374ebf31ea92c145f04d709bc1c4c97fe776
NorthwesternDirector <fanshundee@126.com>
2019-11-30 10:59:56 +0800

    feat: 学习记录 添加时间柱状图

--- src/pages/learningContent/index.jsx
+++ src/pages/learningContent/index.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useMemo } from 'react'
-import { Card, Row, Col, Skeleton } from 'antd'
+import { Card, Row, Col } from 'antd'
 import { connect } from 'dva'
-import echarts from 'echarts'
 import ReactEcharts from 'echarts-for-react'
-import ReactEchartsCore from 'echarts-for-react/lib/core'
-import 'echarts-wordcloud'
+import moment from 'moment'
+import Sunburst from '../../components/Charts/Sunburst'
+import WordCloud from '../../components/Charts/WordCloud'
 
 const LearningContent = ({
   loading,
   learnInfo,
   learnWords,
+  learnTimes,
   dispatch,
 }) => {
   useEffect(() => {
@@ -24,121 +25,104 @@ const LearningContent = ({
     })
   }, [])
 
-  const Sunburst = ({ data, chartHeight }) => {
-    const option = {
-      series: {
-        type: 'sunburst',
-        highlightPolicy: 'ancestor',
-        data,
-        radius: [0, '95%'],
-        sort: null,
-        levels: [{}, {
-            r0: '15%',
-            r: '35%',
-            itemStyle: {
-                borderWidth: 2,
-            },
-            label: {
-                rotate: 'tangential',
-            },
-        }, {
-            r0: '35%',
-            r: '70%',
-            label: {
-                align: 'right',
-            },
-        }, {
-            r0: '70%',
-            r: '72%',
-            label: {
-                position: 'outside',
-                padding: 3,
-                silent: false,
-            },
-            itemStyle: {
-                borderWidth: 3,
-            },
-        }],
-      },
-    }
-    return <ReactEcharts theme="theme" option={option} style={{ height: chartHeight }}></ReactEcharts>
-  }
+  useEffect(() => {
+    dispatch({
+      type: 'learningContent/fetchLearningTimes',
+    })
+  }, [])
+  const selectminute = useMemo(() =>
+    new Array(960).fill(1).map((val, index, array) =>
+      moment('2019-11-28 23:00').subtract(array.length - index, 'minute').format('HH:mm'),
+    ), [])
 
-  const WordCloud = ({ data, onEvents, chartHeight = 300 }) => {
-    if (!data) return <Skeleton title={false} />
-    const formatData = data
-    if (formatData.length === 0) return <Skeleton title={false} />
+  const inRangeminute = useMemo(() =>
+  new Array(20).fill(1).map((val, index, array) =>
+    moment('2019-11-28 09:10').subtract(array.length - index, 'minute').format('HH:mm'),
+  ), [])
+  console.log(inRangeminute)
+
+
+  const Bar = ({
+    data,
+    chartHeight = 600,
+    xoffset = 0,
+  }) => {
     const option = {
       tooltip: {
-        show: false,
-        trigger: 'item',
-        formatter(params) {
-          return `${params.name}:${params.value}`
+        trigger: 'axis',
+        formatter: '<div style="border-bottom: 1px solid rgba(255,255,255,.3); font-size: 16px;padding-bottom: 3px;margin-bottom: 3px">{b0}</div>{a0}: {c0}<br />{a1}: {c1}<br />{a2}: {c2}h',
+      },
+      legend: {
+        data: ['上班时间', '下班时间', '工作时长'],
+      },
+      xAxis: {
+          type: 'category',
+          data: data && data[0].list.map(item => item.date),
+          offset: xoffset,
+      },
+      yAxis: [{
+          type: 'category',
+          data: selectminute,
+          name: '时间',
+      }, {
+        type: 'value',
+        scale: true,
+        name: '工作时长/h',
+        max: 12,
+        min: 0,
+      }],
+      series: [{
+        name: '上班时间',
+        data: data && data[0].list.map(item => ({ name: item.date, value: item.time[0].slice(0, 5) })),
+        type: 'line',
+        smooth: true,
+        color: '#ffde33',
+        markLine: {
+          silent: true,
+          data: [
+              { yAxis: '09:00' },
+          ],
+      },
+      }, {
+        name: '下班时间',
+        data: data && data[0].list.map(item => ({ name: item.date, value: item.time[1].slice(0, 5) })),
+        type: 'line',
+        smooth: true,
+        color: '#0000CD',
+        markLine: {
+          silent: true,
+          data: [
+              { yAxis: '18:30' },
+              { yAxis: '21:00' },
+          ],
         },
       },
-
-      series: [
-        {
-          type: 'wordCloud',
-          shape: 'circle',
-          maskImage: null,
-          left: 'center',
-          top: 'center',
-          width: '95%',
-          height: '95%',
-          right: null,
-          bottom: null,
-          sizeRange: [12, 60],
-          rotationRange: [0, 0],
-          rotationStep: 0,
-          gridSize: 8,
-          drawOutOfBound: false,
-          textStyle: {
-            normal: {
-              fontFamily: 'sans-serif',
-              fontWeight: 'bold',
-              color() {
-                return `rgb(${[
-                  Math.round(Math.random() * 200),
-                  Math.round(Math.random() * 200),
-                  Math.round(Math.random() * 200),
-                ].join(',')})`
-              },
-            },
-            emphasis: {
-              shadowBlur: 2,
-              shadowColor: '#333',
-            },
-          },
-          // Data is an array. Each array item must have name and value property.
-          data: formatData,
-        },
+      {
+        name: '工作时长',
+        data: data && data[0].list.map(item => ({ name: item.date, value: item.time[2] })),
+        type: 'bar',
+        color: '#006699',
+        yAxisIndex: 1,
+      },
       ],
     }
-    return (
-      <ReactEchartsCore
-        theme="theme"
-        echarts={echarts}
-        option={option}
-        notMerge
-        onEvents={onEvents}
-        style={{ height: chartHeight }}
-      />
-    )
+
+    return <ReactEcharts theme="theme" option={option} style={{ height: chartHeight }}></ReactEcharts>
   }
-
-
   return (
     <>
     <Row gutter={24}>
       <Col span={12}>
-        <Card title="在xiaomi实习阳光灿烂的日子">
-          <Sunburst data={learnInfo.data} chartHeight={600}></Sunburst>
+        <Card title="在实习阳光灿烂的日子">
+          <Sunburst data={learnInfo.data} chartHeight={570}></Sunburst>
         </Card>
         </Col>
         <Col span={12}>
-        <Card title="在xiaomi实习阳光灿烂的日子">
-        <WordCloud data={learnWords.data}></WordCloud>
+        <Card title="在实习阳光灿烂的日子">
+        <WordCloud data={learnWords.data} chartHeight={195}></WordCloud>
+        </Card>
+        <Card title="在实习阳光灿烂的日子" style={{ marginTop: 5 }}>
+        <Bar data={learnTimes.data} chartHeight={265} ></Bar>
         </Card>
       </Col>
     </Row>
@@ -149,5 +133,6 @@ const LearningContent = ({
 export default connect(({ learningContent, loading }) => ({
   learnInfo: learningContent.learnInfo,
   learnWords: learningContent.learnWords,
+  learnTimes: learningContent.learnTimes,
   loading: loading.effects['learningContent/fetchLearningContent'],
 }))(LearningContent)
