5667393a0135d019a0f043160ebcdd3bfeedcdc9
NorthwesternDirector <fanshundee@126.com>
2019-12-01 12:26:00 +0800

    feat: 修改登录页面样式

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -5,7 +5,7 @@ import logo from '../assets/welcome.JPG';
 
 export default () => {
   const IconFont = Icon.createFromIconfontCN({ // iconfront 字体导入
-    scriptUrl: '//at.alicdn.com/t/font_1538571_tdnvtxl3pzd.js',
+    scriptUrl: '//at.alicdn.com/t/font_1538571_hi4e0viilxc.js',
   })
   return (
   <PageHeaderWrapper title="ND学习时间" subTitle="我又不是个薯憨憨，当然会选择做前端">
@@ -55,24 +55,32 @@ export default () => {
         </Card>
       </Col>
       <Col span={8}>
-        <Card bodyStyle={{ paddingBottom: 0 }}>
-        <Timeline>
-          <Timeline.Item dot={ <IconFont type="icon-xuexiaogaikuang" />}>2013-09-01 中国地质大学（武汉）</Timeline.Item>
-          <Timeline.Item dot={<IconFont type="icon-xuesheng" />}> 院足球队队长 2015-09-01</Timeline.Item>
-          <Timeline.Item dot={<IconFont type="icon-xuesheng" />}> 团学联副主席 2016-09-01</Timeline.Item>
-          <Timeline.Item dot={<IconFont type="icon-zuqiu-copy" />}> 全国大学生七人制足球赛湖北省季军</Timeline.Item>
-          <Timeline.Item dot={<IconFont type="icon-xuexiaogaikuang" />}>2017-09-01 中国海洋大学</Timeline.Item>
-          <Timeline.Item dot={<IconFont type="icon-zuqiu-copy" />}> 17-18海大“校庆杯”足球赛冠军</Timeline.Item>
-          <Timeline.Item dot={<IconFont type="icon-gongzuo" />}> 植物物候观测系统web地图模块开发</Timeline.Item>
-          <Timeline.Item dot={<IconFont type="icon-xuesheng" />}> 校足球队队长 2018-07-01</Timeline.Item>
-          <Timeline.Item dot={<IconFont type="icon-gongzuo" />}> 校园公共服务系统开发</Timeline.Item>
-          <Timeline.Item dot={<IconFont type="icon-gongzuozheng" />}> 青勘院实习 2018-10-10</Timeline.Item>
-          <Timeline.Item dot={<IconFont type="icon-zuqiu-copy" />}> 18-19海大“校庆杯”足球赛冠军</Timeline.Item>
-          <Timeline.Item dot={<IconFont type="icon-gongzuo" />}> 档案馆web地图模块开发</Timeline.Item>
-          <Timeline.Item dot={<IconFont type="icon-lunwenfabiaoqingkuang" />}> 中国海洋大学学报（自然科学版）《基于聚类算法的出租车载客点时空分布特征研究》</Timeline.Item>
-          <Timeline.Item dot={<IconFont type="icon-gongzuozheng" /> } style={{ minHeight: '21' }}> 小米实习 2019-11-04</Timeline.Item>
-
-        </Timeline>
+        <Card bodyStyle={{ padding: '10px 0' }}>
+          <Collapse bordered={false} style={{ border: 0 }}>
+            <Collapse.Panel header="2013 中国地质大学（武汉）地理信息科学" key="1" style={{ border: 0 }}>
+              <Timeline style={{ marginLeft: '25px' }}>
+              <Timeline.Item dot={<IconFont type="icon-aixin" />}> 脱离单生狗 2015-04-25</Timeline.Item>
+                <Timeline.Item dot={<IconFont type="icon-xuesheng" />}> 院足球队队长 2015-09-01</Timeline.Item>
+                <Timeline.Item dot={<IconFont type="icon-xuesheng" />}> 团学联副主席 2016-09-01</Timeline.Item>
+                <Timeline.Item dot={<IconFont type="icon-zuqiu-copy" />}> 全国大学生七人制足球赛湖北省季军</Timeline.Item>
+              </Timeline>
+            </Collapse.Panel>
+          </Collapse>
+          <Collapse bordered={false} style={{ border: 0 }}>
+            <Collapse.Panel header="2017 中国海洋大学 测绘工程" key="2" style={{ border: 0 }}>
+              <Timeline style={{ marginLeft: '25px' }}>
+              <Timeline.Item dot={<IconFont type="icon-zuqiu-copy" />}> 17-18海大“校庆杯”足球赛冠军</Timeline.Item>
+                <Timeline.Item dot={<IconFont type="icon-gongzuo" />}> 植物物候观测系统web地图模块开发</Timeline.Item>
+                <Timeline.Item dot={<IconFont type="icon-xuesheng" />}> 校足球队队长 2018-07-01</Timeline.Item>
+                <Timeline.Item dot={<IconFont type="icon-gongzuo" />}> 校园公共服务系统开发</Timeline.Item>
+                <Timeline.Item dot={<IconFont type="icon-gongzuozheng" />}> 青勘院实习 2018-10-10</Timeline.Item>
+                <Timeline.Item dot={<IconFont type="icon-zuqiu-copy" />}> 18-19海大“校庆杯”足球赛冠军</Timeline.Item>
+                <Timeline.Item dot={<IconFont type="icon-gongzuo" />}> 档案馆web地图模块开发</Timeline.Item>
+                <Timeline.Item dot={<IconFont type="icon-lunwenfabiaoqingkuang" />}> 中国海洋大学学报（自然科学版）《基于聚类算法的出租车载客点时空分布特征研究》</Timeline.Item>
+                <Timeline.Item dot={<IconFont type="icon-gongzuozheng" /> } style={{ minHeight: '21' }}> 小米实习 2019-11-04</Timeline.Item>
+              </Timeline>
+            </Collapse.Panel>
+          </Collapse>
         </Card>
       </Col>
     </Row>
