294ed68aea97103529c4e19f3c6f79ad5b89b1e3
fanshun <fanshun@xiaomi.com>
2020-01-06 16:01:20 +0800

    feat: bug修复及添加三条数据

--- src/pages/learningContent/index.jsx
+++ src/pages/learningContent/index.jsx
@@ -31,16 +31,14 @@ const LearningContent = ({
     })
   }, [])
   const selectminute = useMemo(() =>
-    new Array(960).fill(1).map((val, index, array) =>
-      moment('2019-11-28 23:00').subtract(array.length - index, 'minute').format('HH:mm'),
+    new Array(1120).fill(1).map((val, index, array) =>
+      moment('2019-11-29 01:00').subtract(array.length - index, 'minute').format('HH:mm'),
     ), [])
 
   const inRangeminute = useMemo(() =>
   new Array(20).fill(1).map((val, index, array) =>
     moment('2019-11-28 09:10').subtract(array.length - index, 'minute').format('HH:mm'),
   ), [])
-  console.log(inRangeminute)
-
 
   const Bar = ({
     data,
@@ -50,10 +48,16 @@ const LearningContent = ({
     const option = {
       tooltip: {
         trigger: 'axis',
-        formatter: '<div style="border-bottom: 1px solid rgba(255,255,255,.3); font-size: 16px;padding-bottom: 3px;margin-bottom: 3px">{b0}</div>{a0}: {c0}<br />{a1}: {c1}<br />{a2}: {c2}h',
+        backgroundColor: 'rgba(245, 245, 245, 0.8)',
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 10,
+        textStyle: {
+            color: '#000',
+        },
       },
       legend: {
-        data: ['上班时间', '下班时间', '工作时长'],
+        data: ['上班时间', '下班时间', '入睡时间', '工作时长', '睡眠时长', '深睡时长'],
       },
       xAxis: {
           type: 'category',
@@ -76,13 +80,13 @@ const LearningContent = ({
         data: data && data[0].list.map(item => ({ name: item.date, value: item.time[0].slice(0, 5) })),
         type: 'line',
         smooth: true,
-        color: '#ffde33',
+        color: '#9400D3',
         markLine: {
           silent: true,
           data: [
               { yAxis: '09:00' },
           ],
-      },
+        },
       }, {
         name: '下班时间',
         data: data && data[0].list.map(item => ({ name: item.date, value: item.time[1].slice(0, 5) })),
@@ -101,7 +105,28 @@ const LearningContent = ({
         name: '工作时长',
         data: data && data[0].list.map(item => ({ name: item.date, value: item.time[2] })),
         type: 'bar',
+        color: '#B0E0E6',
+        yAxisIndex: 1,
+      },
+      {
+        name: '入睡时间',
+        data: data && data[0].list.map(item => ({ name: item.date, value: item.sleepTime[0] })),
+        type: 'line',
+        smooth: true,
+        color: '#006400',
+      },
+      {
+        name: '睡眠时长',
+        data: data && data[0].list.map(item => ({ name: item.date, value: item.sleepTime[1] })),
+        type: 'bar',
         color: '#006699',
+        yAxisIndex: 1,
+      },
+      {
+        name: '深睡时长',
+        data: data && data[0].list.map(item => ({ name: item.date, value: item.sleepTime[2] })),
+        type: 'bar',
+        color: '#FFD700',
         yAxisIndex: 1,
       },
       ],
@@ -112,18 +137,21 @@ const LearningContent = ({
   return (
     <>
     <Row gutter={24}>
-      <Col span={12}>
+      <Col span={24} style={{ marginBottom: 24 }}>
+        <Card title="在实习阳光灿烂的日子" style={{ marginTop: 5 }}>
+        <Bar data={learnTimes.data} chartHeight={570} ></Bar>
+        </Card>
+      </Col>
+      <Col span={14}>
         <Card title="在实习阳光灿烂的日子">
           <Sunburst data={learnInfo.data} chartHeight={570}></Sunburst>
         </Card>
-        </Col>
-        <Col span={12}>
-        <Card title="在实习阳光灿烂的日子">
-        <WordCloud data={learnWords.data} chartHeight={195}></WordCloud>
-        </Card>
-        <Card title="在实习阳光灿烂的日子" style={{ marginTop: 5 }}>
-        <Bar data={learnTimes.data} chartHeight={265} ></Bar>
-        </Card>
+      </Col>
+
+      <Col span={10}>
+      <Card title="在实习阳光灿烂的日子">
+        <WordCloud data={learnWords.data} chartHeight={300}></WordCloud>
+      </Card>
       </Col>
     </Row>
     </>
