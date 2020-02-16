224026439ff5040eeb16c02c3639f011e02e29df
fanshun <fanshundee@126.com>
2020-02-16 11:02:16 +0800

    fix: 图表说明更新

--- src/pages/newYear2020/index.jsx
+++ src/pages/newYear2020/index.jsx
@@ -196,7 +196,7 @@ const LearningContent = ({
       title: {
         top: 0,
         text: title,
-        subtext: '信息来自国家卫生健康委员会官方网站 (http://www.nhc.gov.cn) , 数据更新时间: 2020/02/15 09:00',
+        subtext: '信息来自国家卫生健康委员会官方网站 (http://www.nhc.gov.cn) , 数据更新时间: 2020/02/16 09:00',
         sublink: 'http://www.nhc.gov.cn',
         left: 'center',
       },
@@ -219,7 +219,7 @@ const LearningContent = ({
         },
       },
       legend: {
-        data: ['新增死亡', '新增治愈', '新增确诊', '新增疑似', '累计死亡', '累计治愈', '累计确诊', '累计疑似', '追踪密切接触者', '解除观察', '尚在医学观察者'],
+        data: ['新增死亡', '新增治愈', '新增确诊', '新增疑似', '累计死亡', '累计治愈', '现有确诊', '累计疑似', '追踪密切接触者', '解除观察', '尚在医学观察者'],
         top: 50,
       },
       xAxis: {
@@ -266,7 +266,7 @@ const LearningContent = ({
         color: '#FFB5C5',
         yAxisIndex: 1,
       }, {
-        name: '累计确诊',
+        name: '现有确诊',
         data: data.map(item => item.totalConfirmedCase),
         type: 'line',
         smooth: true,
@@ -310,7 +310,7 @@ const LearningContent = ({
       },
       ],
       grid: {
-        left: '40',
+        left: '50',
         top: '120',
         right: '150',
       },
@@ -422,7 +422,8 @@ const LearningContent = ({
           <Col span={21}>
             <VirusBar data={virus.data} chartHeight={600} title="新型冠状病毒肺炎疫情通报"/>
             <div style={{ fontSize: 12, color: '#999', marginTop: -20, textAlign: 'center' }}>
-              <p>说明：‘新增死亡’、‘新增治愈’参考坐标轴低1；‘新增确诊’、‘新增疑似’、‘累计死亡’、‘累计治愈’参考坐标轴中2；‘累计确诊‘、‘累计疑似‘、‘追踪密切接触者‘、‘尚在医学观察者‘参考坐标轴高3</p>
+              <p>说明：1. ‘新增死亡’、‘新增治愈’参考坐标轴低1；‘新增确诊’、‘新增疑似’、‘累计死亡’、‘累计治愈’参考坐标轴中2；‘现有确诊‘、‘累计疑似‘、‘追踪密切接触者‘、‘尚在医学观察者‘参考坐标轴高3</p>
+              <p>2. 为了更加直观展示确诊人数，图中仅展示现有确诊人数不展示累计确诊人数，计算关系为：‘现有确诊’ = ‘累计确诊’ - ‘累计治愈’ - ‘累计死亡’</p>
             </div>
           </Col>
           <Col span={3}>
